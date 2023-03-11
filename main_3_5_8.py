64367671d933649fb676c6b3d2925d9ae0d3e55e
running-poe <djunkovsky.p@gmail.com>
2023-03-11 13:15:46 +0300

    запили генерацию тестов нормальную

--- main_3_5_8.py
+++ main_3_5_8.py
@@ -1,10 +1,6 @@
 
 # 3.5 - 8
 from datetime import datetime
-from pytest_gen import StepikTestGenerator
-
-t = StepikTestGenerator('input_test_3.5.8', 'output_test_3.5.8', 'test.py', 'main_3_5_8', 'func')
-t.doit()
 
 
 
---
+++ run.py
@@ -0,0 +1,4 @@
+from pytest_gen import StepikTestGenerator
+
+t = StepikTestGenerator('input_test_3.5.8', 'output_test_3.5.8', 'test.py', 'main_3_5_8', 'func')
+t.doit()
